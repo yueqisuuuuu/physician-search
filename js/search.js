31eb1e39d47445a7cfbfc3ddad76a3d7966e82b7
Yueqi Su <ysu@scu.edu>
2017-09-03 11:30:55 -0700

    js load version

--- js/search.js
+++ js/search.js
@@ -1,51 +1,29 @@
+// preprocess data onload 
 window.onload = function(){
   initMap();
+  
   $.get('data/OP_DTL_OWNRSHP_PGYR2016_P06302017.csv', function(data){
-  $('#result').append(arrayToTable(Papa.parse(data).data));
+  processData(Papa.parse(data,{
+    header: true
+  }).data);
 });
 }
 
-
-function arrayToTable(tableData) {
-    var table = $('<table></table>');
-    $(tableData).each(function (i, rowData) {
-        var row = $('<tr></tr>');
-        $(rowData).each(function (j, cellData) {
-            row.append($('<td>'+cellData+'</td>'));
-        });
-        table.append(row);
-    });
-    return table;
+// load all data
+function processData(csvData) {
+  console.log(csvData);
+    
 }
 
+function jsSearch(){
+  // search validation
+    var lastname = $("#lname").val();
+    if (lastname == "") {
+        document.getElementById("error").innerHTML = "*Last Name Required";
+        return false;
+     }
+     else{
+        document.getElementById("error").innerHTML = "";
+     }
+}
 
-/*
-$.ajax({
-        type: "GET",
-        url: "data/OP_DTL_OWNRSHP_PGYR2016_P06302017.csv",
-        dataType: "text",
-        success: function (data) {
-            $('#result').append(arrayToTable(Papa.parse(data).data));
-        },
-        error: function (jqXHR, exception) {
-            var msg = '';
-            if (jqXHR.status === 0) {
-              msg = 'Not connect.\n Verify Network.';
-            } else if (jqXHR.status == 404) {
-              msg = 'Requested page not found. [404]';
-            } else if (jqXHR.status == 500) {
-              msg = 'Internal Server Error [500].';
-            } else if (exception === 'parsererror') {
-              msg = 'Requested JSON parse failed.';
-            } else if (exception === 'timeout') {
-              msg = 'Time out error.';
-            } else if (exception === 'abort') {
-              msg = 'Ajax request aborted.';
-            } else {
-              msg = 'Uncaught Error.\n' + jqXHR.responseText;
-            }
-            console.log(msg);
-          }
-    });
-
-    */
